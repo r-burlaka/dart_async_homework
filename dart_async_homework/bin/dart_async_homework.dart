b3b1d6a8191323a4be769b487a44c4b8aa1ca2cb
Rostyslav Burlaka <ukrdentist@gmail.com>
2025-07-13 16:32:34 +0300

    feat: initial commit

---
+++ dart_async_homework/bin/dart_async_homework.dart
@@ -0,0 +1,5 @@
+import 'package:dart_async_homework/dart_async_homework.dart' as dart_async_homework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_async_homework.calculate()}!');
+}
---
+++ dart_async_homework/test/dart_async_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_async_homework/dart_async_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
