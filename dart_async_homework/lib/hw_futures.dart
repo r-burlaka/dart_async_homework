fc1ebf2131ac14d39cbb6cb56f044946348b5023
Rostyslav Burlaka <ukrdentist@gmail.com>
2025-07-13 16:47:34 +0300

    feat Future task_5

--- dart_async_homework/lib/hw_futures.dart
+++ dart_async_homework/lib/hw_futures.dart
@@ -1,35 +1,14 @@
-void main() async { 
-  var paralelno = await Future.wait ([fetchName(), fetchAge()]);
-  final stopwatch = Stopwatch();
-  stopwatch.start(); 
-  String myName = await fetchName();
-  print(stopwatch.elapsedMilliseconds);
-  stopwatch.stop();
-  print('Мене звати - $myName');
+import 'dart:async';
 
-  String roky;
-  stopwatch.reset();
-  stopwatch.start();  
-  roky = await fetchAge ();
-  print(stopwatch.elapsedMilliseconds);
-  stopwatch.stop(); 
-  int vik = int.parse(roky);
-  if  (vik == 1) { print ('Мій вік $vik років');}
-   else if ( 2 <= vik && vik <= 4) {print ('Мій вік $vik роки');}
-    else if ( vik % 100 >= 11 && vik % 100 <= 14) {print ('Мій вік $vik років');} 
-    else if (vik % 10 == 1) {print ('Мій вік $vik рік');}
-     else if (vik % 10 == 2 || vik % 10 == 3 || vik % 10 == 4) {print ('Мій вік $vik роки');}
-      else {print ( 'Мій вік $vik років');}
-  
+Future<String> delayedCountdown(int seconds) async {
+  for (int i = seconds; i > 0; i--) {
+    print('$i...');
+    await Future.delayed(Duration(seconds: 1));
+  }
+  return 'Старт!';
 }
 
-Future<String> fetchName() async {
-  await Future.delayed(Duration(seconds: 2));
-  return 'Rostyslav';
+void main() async {
+  String result = await delayedCountdown(3);
+  print(result);
 }
-
-
-Future<String> fetchAge() async { 
-  await Future.delayed(Duration(milliseconds: 1500));  
-  return ('25');
- }
