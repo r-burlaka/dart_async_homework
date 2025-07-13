852392a63507a6886abff5628b6b7cb2c741fecb
Rostyslav Burlaka <ukrdentist@gmail.com>
2025-07-13 16:58:02 +0300

    feat Streams Task_7

--- dart_async_homework/lib/hw_streams.dart
+++ dart_async_homework/lib/hw_streams.dart
@@ -1,15 +1,12 @@
 import 'dart:async';
 
 void main() async {
-  final stream = Stream.fromIterable([1, 2, 3, 4, 5]).asBroadcastStream();
+  final stream = Stream.periodic(
+    Duration(seconds: 1),
+    (index) => index + 1,
+  ).take(10);
 
-  // Варіант 1 — await for
   await for (final value in stream) {
-    print(value);
+    print('$value...');
   }
-
-  // Варіант 2 — listen
-  stream.listen((value) {
-    print(value);
-  });
 }
